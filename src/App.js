3ba15e4014eca0ea22204fdda3a49f5c346ef0c0
TaraWalk <tarawalker1310@gmail.com>
2025-02-18 10:12:58 +0000

    Set up form

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
 
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Tara Walker</footer>
+      </div>
+    </div>
+  );
+}
